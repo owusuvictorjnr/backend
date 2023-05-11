f8bebeddf5bdeab245c9f8582e4719e47d6389eb
owusuvictorjnr <owusuvictor80@gmail.com>
2023-05-11 13:03:44 +0000

    backend api

---
+++ src/data.ts
@@ -0,0 +1,68 @@
+import { Product } from './types/Product';
+
+export const sampleProduct: Product[] = [
+  {
+    name: 'Long Sleev',
+    slug: 'longsleev',
+    category: 'Sleeves',
+    images: '../img/pic1.jpg',
+    price: 50,
+    brand: 'African Print',
+    countInStock: 100,
+    rating: 4.5,
+    description: 'high quality African ware',
+    numReviews: 10,
+  },
+
+  {
+    name: 'Short Sleev',
+    slug: 'shortsleev',
+    category: 'Sleeves',
+    images: '../img/pic2.jpg',
+    price: 150,
+    brand: 'African Print',
+    countInStock: 100,
+    rating: 4.5,
+    description: 'high quality African ware',
+    numReviews: 10,
+  },
+
+  {
+    name: 'beauty',
+    slug: 'African beauty',
+    category: 'beauty',
+    images: '../img/pic3.jpg',
+    price: 150,
+    brand: 'African Print',
+    countInStock: 20,
+    rating: 4.5,
+    description: 'high quality African ware',
+    numReviews: 10,
+  },
+
+  {
+    name: 'cute',
+    slug: 'cutetness',
+    category: 'beauty',
+    images: '../img/pic4.jpg',
+    price: 1150,
+    brand: 'African Print',
+    countInStock: 100,
+    rating: 4.5,
+    description: 'high quality African ware',
+    numReviews: 10,
+  },
+
+  {
+    name: 'Pants',
+    slug: 'African Pants',
+    category: 'Pants',
+    images: '../img/pic5.jpg',
+    price: 50,
+    brand: 'African Print',
+    countInStock: 100,
+    rating: 4.5,
+    description: 'high quality African ware',
+    numReviews: 10,
+  },
+];
