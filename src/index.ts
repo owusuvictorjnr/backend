0f0cd77c5cb67b18e9c791f3481749182d0aa3d5
owusuvictorjnr <owusuvictor80@gmail.com>
2023-05-12 17:20:32 +0000

    create a backend api

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,9 @@ app.get('/api/products', (req: Request, res: Response) => {
   res.json(sampleProduct);
 });
 
+app.get('/api/products/:slug', (req: Request, res: Response) => {
+  res.json(sampleProduct.find((x) => x.slug === req.params.slug));
+});
 const PORT = 4000;
 app.listen(PORT, () => {
   console.log(`server started http://localhost:${PORT}`);
